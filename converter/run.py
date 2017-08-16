00c86048411dd8764d4eab0fb074bf82d22b23e1
Thomas Goodwin <btgoodwin@geontech.com>
2017-08-16 12:06:55 -0400

    1. Cleaned up interface to create_xmls main by removing implementation and generator, which are both only used internally (not the calling app). 2. Added options for docker_image and docker_volume to run.py 3. Added the new docker options to the create_xmls main and formatSPD 4. Then implemented propertyRef dependencies conditionally to support Docker-GPP

--- converter/run.py
+++ converter/run.py
@@ -13,6 +13,7 @@ import re
 import subprocess
 import sys
 import logging
+from optparse import OptionParser, OptionGroup
 
 import lib.create_xmls as cx
 import lib.jinja_file_edits as jfe
@@ -22,9 +23,6 @@ from lib.xml_parsing import XMLParsing
 LOGGER_NAME = "GRComponentConverter"
 
 # ##############################################################################
-# TODO: Add features for --help. For example, allow --directory flag for
-#       providing a user defined output directory (OPTPARSE)
-#
 # Optional TODO's if Time Exists:
 #
 # TODO : Add option for creating the output_dir path if it does not exist.
@@ -107,17 +105,16 @@ def main(grc_file, destination, options):
     # modification to the created "Makefile.am.ide" file, followed by a shell
     # process that moves the "top_block.py" (or similar) file inside the
     # newly generated directory.
-    #
-    # TODO: This generator variable may need to be changed depending on what the
-    #       actual implementation name of that directory will become, or if it
-    #       will actually be present in redhawk's "pull" template, then you can
-    #       make use of the commented "old_generator" variable below.
     # ##########################################################################
-    generator = "python.component.gr_flowgraph"
-
-    respkg = cx.main(trimmed_grc_name, "python", output_dir,
-        generator, parsed_grc.properties_array,
-        parsed_grc.source_types, parsed_grc.sink_types, grc_input, py_path)
+    respkg = cx.main(
+        name =          trimmed_grc_name,
+        output_dir =    output_dir,
+        prop_array =    parsed_grc.properties_array,
+        source_types =  parsed_grc.source_types,
+        sink_types =    parsed_grc.sink_types,
+        grc_input =     grc_input,
+        docker_image =  options.docker_image,
+        docker_volume = options.docker_volume)
 
     jfe.main(parsed_grc.python_file_name, trimmed_grc_name, parsed_grc.properties_array, respkg["pd"])
 
@@ -130,7 +127,7 @@ def main(grc_file, destination, options):
 
 
 if __name__ == '__main__':
-    from optparse import OptionParser
+    # Options parsing
     parser = OptionParser()
     parser.usage = "%prog [options] GRC_FILE [DESTINATION]"
     parser.add_option(
@@ -139,6 +136,24 @@ if __name__ == '__main__':
         dest="verbose",
         default=False,
         action="store_true")
+
+    group = OptionGroup(parser,
+        "Docker-GPP Support",
+        "Note: These options will make your Component require the Docker-GPP!")
+    group.add_option(
+        "--docker-image",
+        help="Docker Image name that will house this component",
+        dest="docker_image",
+        default=None)
+    group.add_option(
+        "--docker-volume",
+        help="Docker Volume required by this component (repeat if multiple)",
+        dest="docker_volume",
+        action="append",
+        type="string",
+        default=None)
+    parser.add_option_group(group)
+
     (options, args) = parser.parse_args()
 
     # Setup logging
@@ -146,6 +161,11 @@ if __name__ == '__main__':
     if options.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     _log = logging.getLogger(LOGGER_NAME)
+
+    # Validate 
+    if options.docker_volume and not options.docker_image:
+        _log.error("If a volume is specified, then an image must be specified.");
+        sys.exit(1)
 
     num_args = len(args)
     if num_args == 0:
